43df6749374bb49256f86060b3f054908b582e05
Edomiyas Temesgen <edomiyas.temesgen@x-pans.com>
2021-09-11 01:42:54 +0200

    Tip calculator works but not customized percentage

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ let tenPercent = document.getElementById("ten-percent");
 let fifteenPercent = document.getElementById("fifteen-percent");
 let twentyFivePercent = document.getElementById("twenty-five-percent");
 let fiftyPercent = document.getElementById("fifty-percent");
-let customPercent = document.getElementById("custom");
 let tipAmount =  document.getElementById("tip-amount-num");
 let totalAmount =  document.getElementById("total-amount");
 let error = document.getElementById("error");
 
 function getBill() {
     let tip = 0;
-    billAmount = document.querySelector('input').value;
+    billAmount = document.querySelector('#bill-amount').value;
     console.log(billAmount);
+
+/*    numberPeople = document.querySelector('#number-people-amount').value;
+    console.log(numberPeople);
+
+    if(numberPeople == 0){
+        error.innerHTML = "Can't be zero";
+        numberPeople.style.border = ".15rem solid rgb(221, 52, 52)";
+    }*/
+
+/*    customPercent = document.querySelector('#custom').value;
+    console.log(customPercent);*/
 
     if(billAmount){
         calculateTipFivePercent();
@@ -20,73 +30,68 @@ function getBill() {
         calculateTipTwentyFivePercent();
         calculateTipFiftyPercent();
         calculateTipCustomPercent();
-    //    totalFivePercent();
     }
 
 
     function calculateTipFivePercent(){
         fivePercent.addEventListener("click", () => {
-            tip = tipAmount.innerHTML = Number((billAmount / 5).toFixed(1));
-            totalAmount.innerHTML = +billAmount + tip;
+            tip = tipAmount.innerHTML = Number((billAmount * 5/100) / getPeople()).toFixed(1);
+            totalAmount.innerHTML = (+billAmount / +tip).toFixed(1);
         })
     }
-    //calculateTipFivePercent();
 
     function calculateTipTenPercent(){
         tenPercent.addEventListener("click", () => {
-            tip = tipAmount.innerHTML = Number((billAmount / 10).toFixed(1));
-            totalAmount.innerHTML = +billAmount + tip;
+            tip = tipAmount.innerHTML = Number((billAmount * 10/100) / getPeople()).toFixed(1);
+            totalAmount.innerHTML = (+billAmount / tip).toFixed(1);
         })  
     }
-    //calculateTipTenPercent();
 
     function calculateTipFifteenPercent(){
         fifteenPercent.addEventListener("click", () => {
-            tip = tipAmount.innerHTML = Number((billAmount / 15).toFixed(1));
-            totalAmount.innerHTML = +billAmount + tip;
+            tip = tipAmount.innerHTML = Number((billAmount * 15/100) / getPeople()).toFixed(1);
+            totalAmount.innerHTML = (+billAmount / tip).toFixed(1);
         })
     }
-    //calculateTipFifteenPercent();
 
     function calculateTipTwentyFivePercent(){
         twentyFivePercent.addEventListener("click", () => {
-            tip = tipAmount.innerHTML = Number((billAmount / 25).toFixed(1));
-            totalAmount.innerHTML = +billAmount + tip;
+            tip = tipAmount.innerHTML = Number((billAmount * 25/100) / getPeople()).toFixed(1);
+            totalAmount.innerHTML = (+billAmount / tip).toFixed(1);
         })
     }
-    //calculateTipTwentyFivePercent();
 
     function calculateTipFiftyPercent(){
         fiftyPercent.addEventListener("click", () => {
-            tip = tipAmount.innerHTML = Number((billAmount / 50).toFixed(1));
-            totalAmount.innerHTML = +billAmount + tip;
+            tip = tipAmount.innerHTML = Number((billAmount * 50/100) / getPeople()).toFixed(1);
+            totalAmount.innerHTML = (+billAmount / tip).toFixed(1);
         })
     }
-    //calculateTipFiftyPercent();
 
     function calculateTipCustomPercent(){
         customPercent.addEventListener("click", () => {
-            tip = tipAmount.innerHTML = Number((billAmount / document.querySelector("custom").value).toFixed(1));
-            totalAmount.innerHTML = +billAmount + tip;
+            tip = tipAmount.innerHTML = Number((billAmount * getCustomPercent()) / getPeople()).toFixed(1);
+            totalAmount.innerHTML = (+billAmount / tip).toFixed(1);
         })
     }
-    //calculateTipCustomPercent();
-
-  /*  function totalFivePercent(){
-        totalAmount.innerHTML = "$" + billAmount + tip;
-    }*/
-    //total();
 }
 
-/*
-
 function getPeople(){
-    numberPeople = document.querySelector('input').value;
+    numberPeople = document.querySelector('#number-people-amount').value;
     console.log(numberPeople);
 
     if(numberPeople == 0){
         error.innerHTML = "Can't be zero";
         document.getElementById("number-people-amount").style.border = ".15rem solid rgb(221, 52, 52)";
     }
+
+    return numberPeople;
 }
-*/
+
+function getCustomPercent(){
+    customPercent = document.querySelector('#custom').value;
+    console.log(customPercent);
+
+    return customPercent;
+}
+
